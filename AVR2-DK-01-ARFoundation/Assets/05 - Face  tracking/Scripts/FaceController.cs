ea18f5ea1485b4691f9175f6ffd34b8b393ecd16
David Perret <david.perret@gmail.com>
2020-03-25 14:39:14 +0100

    Update after AVR2 day 3
    - Image tracking
    - Face tracking
    - Light estimation

---
+++ AVR2-DK-01-ARFoundation/Assets/05 - Face  tracking/Scripts/FaceController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[Serializable]
+public class FaceMaterial
+{
+    public Material material;
+    public string name;
+}
+
+public class FaceController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Toggle faceTrackingToggle;
+    [SerializeField]
+    private Button swapFacesButton;
+    [SerializeField]
+    private FaceMaterial[] faceMaterials;
+
+    private ARFaceManager arFaceManager;
+    private bool faceTrackingOn = true;
+    private int swapCounter = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        arFaceManager = GetComponent<ARFaceManager>();
+        arFaceManager.facePrefab.GetComponent<MeshRenderer>().material = faceMaterials[0].material;
+        swapFacesButton.GetComponentInChildren<Text>().text = $"Swap faces ({faceMaterials[0].name})";
+        faceTrackingToggle.GetComponentInChildren<Text>().text = $"Face tracking {(arFaceManager.enabled ? "ON" : "OFF")}";
+    }
+
+    public void SwapFaces()
+    {
+        swapCounter = (swapCounter == faceMaterials.Length - 1) ? 0 : swapCounter + 1;
+        /*if (swapCounter == faceMaterials.Length - 1)
+            swapCounter = 0;
+        else
+            swapCounter++;
+        */
+        foreach(ARFace face in arFaceManager.trackables)
+            face.GetComponent<MeshRenderer>().material = faceMaterials[swapCounter].material;
+
+        swapFacesButton.GetComponentInChildren<Text>().text = $"Swap faces ({faceMaterials[swapCounter].name})";
+        //swapFacesButton.GetComponentInChildren<Text>().text = "Swap faces (" + faceMaterials[swapCounter].name + ")";
+    }
+
+    public void ToggleTrackingFaces()
+    {
+        faceTrackingOn = !faceTrackingOn;
+        arFaceManager.enabled = faceTrackingOn;
+        faceTrackingToggle.GetComponentInChildren<Text>().text = $"Face tracking {(arFaceManager.enabled ? "ON" : "OFF")}";
+    }
+}
---
+++ AVR2-DK-01-ARFoundation/Assets/06 - Light estimation/Scripts/LightEstimationController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class LightEstimationController : MonoBehaviour
+{
+    [SerializeField]
+    private ARCameraManager arCameraManager;
+    [SerializeField]
+    private Text brightnessValue;
+    [SerializeField]
+    private Text temperatureValue;
+    [SerializeField]
+    private Text colorCorrectionValue;
+
+    private PlacerController placerController;
+
+    public List<Light> lights;
+
+    void Awake()
+    {
+        placerController = FindObjectOfType<PlacerController>();
+    }
+
+    private void OnEnable()
+    {
+        arCameraManager.frameReceived += FrameUpdated;
+    }
+
+    private void OnDisable()
+    {
+        arCameraManager.frameReceived -= FrameUpdated;
+    }
+
+    private void FrameUpdated(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue)
+        {
+            brightnessValue.text = $"Brightness: {args.lightEstimation.averageBrightness.Value}";
+            placerController.placedObjects[0].GetComponent<Animator>().SetFloat("AverageBrightness", args.lightEstimation.averageBrightness.Value);
+            foreach(Light light in lights)
+            {
+                light.intensity = args.lightEstimation.averageBrightness.Value;
+            }
+        }
+        if (args.lightEstimation.averageColorTemperature.HasValue)
+        {
+            temperatureValue.text = $"Color temperature: {args.lightEstimation.averageColorTemperature.Value}";
+        }
+        if (args.lightEstimation.colorCorrection.HasValue)
+        {
+            colorCorrectionValue.text = $"Color correction: {args.lightEstimation.colorCorrection.Value}";
+        }
+    }
+}
