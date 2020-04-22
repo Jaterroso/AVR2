9c9a6e2f300f61af0ca1a5fc0a00ffdc86cebea6
davidperret <david.perret@gmail.com>
2020-04-22 13:15:53 +0200

    Photon Unity Network project added

---
+++ PUN/Assets/Scripts/AppManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace com.eonreality.pun
+{
+    public class AppManager : MonoBehaviourPunCallbacks
+    {
+        public static AppManager Instance;
+        private GameObject instance;
+
+        [Tooltip("The prefab to use for representing the user")]
+        [SerializeField]
+        private GameObject userPrefab;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Instance = this;
+
+            if (!PhotonNetwork.IsConnected)
+            {
+                SceneManager.LoadScene("AppLauncher");
+                return;
+            }
+            if (userPrefab == null)
+            {
+                Debug.LogError("<Color=Red><b>Missing</b></Color> playerPrefab reference. Please set it up in GameObject 'AppManager'", this);
+            }
+            else
+            {
+                if (UserManager.localUserInstance == null)
+                {
+                    Debug.LogFormat("We are instantiating LocalUser from {0}", SceneManagerHelper.ActiveSceneName);
+
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.userPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                }
+                else
+                    Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #region PUN Callbacks
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.Log("OnPlayerEnteredRoom() " + other.NickName); // not seen if you're the player connecting
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+                LoadArena();
+            }
+        }
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.Log("OnPlayerLeftRoom() " + other.NickName); // seen when other disconnects
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+                LoadArena();
+            }
+        }
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene("AppLauncher");
+        }
+        #endregion
+
+        #region Private Methods
+        private void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("PUNRoomFor" + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+
+        #endregion
+
+        #region Public methods
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        #endregion
+    }
+}
---
+++ PUN/Assets/Scripts/UserUI.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.eonreality.pun
+{
+    public class UserUI : MonoBehaviour
+    {
+        #region Private Fields
+        [Tooltip("UI Text to display Player's Name")]
+        [SerializeField]
+        private Text userNameText;
+
+        [Tooltip("UI Slider to display Player's Health")]
+        [SerializeField]
+        private Slider userHealthSlider;
+        private UserManager target;
+
+        float characterControllerHeight = 0f;
+        Transform targetTransform;
+        Renderer targetRenderer;
+        CanvasGroup _canvasGroup;
+        Vector3 targetPosition;
+        #endregion
+
+        #region Public fields
+        [Tooltip("Pixel offset from the player target")]
+        [SerializeField]
+        private Vector3 screenOffset = new Vector3(0f, 30f, 0f);
+        #endregion
+
+        #region MonoBehaviour Callbacks
+        void Awake()
+        {
+            _canvasGroup = this.GetComponent<CanvasGroup>();
+            this.transform.SetParent(GameObject.Find("ConsoleLog").GetComponent<Transform>(), false);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Reflect the Player Health
+            if (userHealthSlider != null)
+            {
+               userHealthSlider.value = target.userHealth;
+            }
+
+            if (target == null)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+
+        void LateUpdate()
+        {
+            // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
+            if (targetRenderer != null)
+            {
+                this._canvasGroup.alpha = targetRenderer.isVisible ? 1.0f : 0.0f;
+            }
+
+            // #Critical
+            // Follow the Target GameObject on screen.
+            if (targetTransform != null)
+            {
+                targetPosition = targetTransform.position;
+                targetPosition.y += characterControllerHeight;
+                this.transform.position = Camera.main.WorldToScreenPoint(targetPosition) + screenOffset;
+            }
+        }
+
+        #endregion
+
+
+        #region Public Methods
+        public void SetTarget(UserManager _target)
+        {
+            if (_target == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for UserUI.SetTarget.", this);
+                return;
+            }
+            // Cache references for efficiency
+            target = _target;
+            targetTransform = target.GetComponent<Transform>();
+            targetRenderer = target.GetComponent<Renderer>();
+            CharacterController characterController = target.GetComponent<CharacterController>();
+            // Get data from the Player that won't change during the lifetime of this Component
+            if (characterController != null)
+            {
+                characterControllerHeight = characterController.height;
+            }
+            if (userNameText != null)
+            {
+                userNameText.text = target.photonView.Owner.NickName;
+            }
+        }
+
+        #endregion
+
+    }
+}
